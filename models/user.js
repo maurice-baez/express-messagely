5874190607da805c25e5eddac4c85d249ff1c3dd
Michael Paglione <pagman777@gmail.com>
2022-04-18 18:03:13 -0400

    created user model. WIP bugs from test

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const db = require("../db");
+const { NotFoundError } = require("../expressError");
 
 /** User of the site. */
 
@@ -11,12 +12,12 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const timestamp = new Date()
     const result = await db.query(
-      `INSERT INTO users (username, password, first_name, last_name, phone)
-         VALUES
-           ($1, $2, $3, $4, $5)
+      `INSERT INTO users (username, password, first_name, last_name, phone, join_at)
+         VALUES ($1, $2, $3, $4, $5, $6)
          RETURNING username, password, first_name, last_name, phone`,
-      [username, password, first_name, last_name, phone]);
+      [username, password, first_name, last_name, phone, timestamp]);
 
     return result.rows[0];
   }
@@ -28,7 +29,7 @@ class User {
     const result = await db.query(
       `SELECT password
          FROM users
-         WHERE username = $1`,
+         WHERE username=$1`,
       [username]);
     const user = result.rows[0];
 
@@ -38,12 +39,26 @@ class User {
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+
+    const timestamp = new Date()
+
+    await db.query(
+      `UPDATE users
+        SET last_login_at=$1
+        WHERE username=$2`,
+      [timestamp, username]);
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+
+    const results = await db.query(
+      `SELECT username, first_name, last_name
+        FROM users`);
+
+    return results.rows;
   }
 
   /** Get: get user by username
@@ -56,17 +71,33 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+
+    const result = await db.query(
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at
+        FROM users
+        WHERE username=$1`, [username]);
+    const user = result.rows[0];
+    if (!user) throw new NotFoundError(`No such user: ${username}`);
+
+    return user;
   }
 
   /** Return messages from this user.
-   *
-   * [{id, to_user, body, sent_at, read_at}]
-   *
-   * where to_user is
-   *   {username, first_name, last_name, phone}
-   */
+*
+* [{id, to_username, body, sent_at, read_at}]
+*
+* where to_user is
+*   {username, first_name, last_name, phone}
+*/
 
   static async messagesFrom(username) {
+
+    const result = await db.query(
+      `SELECT id, to_username, body, sent_at, read_at
+        FROM messages
+        WHERE from_username=$1`, [username]);
+
+    return result.rows;
   }
 
   /** Return messages to this user.
@@ -78,6 +109,13 @@ class User {
    */
 
   static async messagesTo(username) {
+
+    const result = await db.query(
+      `SELECT id, from_username, body, sent_at, read_at
+        FROM messages
+        WHERE to_username = $1`, [username]);
+
+    return result.rows;
   }
 }
 
